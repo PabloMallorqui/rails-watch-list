f22eaaff2d4e47d841ae302e2242673183959141
Pablo MallorquÃi <pmallorqui@gmail.com>
2023-05-25 12:50:05 +0200

    back-end done, time to do front-end

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,7 +18,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(params[@bookmark.list_id])
+    redirect_to list_path(@bookmark.list)
   end
 
   private
